a005fe240cf653a142a5cd76b55a5d6ac8a87ccc
Hemanth Ravella <hemanthravella1729@gmail.com>
2024-11-16 14:20:24 -0500

    Setup drf spectacular to generate schema and docs using AutoSchema class

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'core',
     'user',
 ]
@@ -133,3 +134,7 @@ AUTH_USER_MODEL = 'user.User'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
