51b082f2a207f408acb36af2bc14ef8a59b85ef1
Hemanth Ravella <hemanthravella1729@gmail.com>
2024-11-15 23:04:52 -0500

    Created view for create using generics
    Created view for create using generics

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,10 @@
 """This is the user views"""
 
 from django.shortcuts import render
+from rest_framework import generics
 
+from app.user.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Creates a new user in the system"""
+    serializer_class = UserSerializer
