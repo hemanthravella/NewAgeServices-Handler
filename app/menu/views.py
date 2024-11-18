7d2a7d2151785a6c6c4f8019555014bc2b470efc
Hemanth Ravella <hemanthravella1729@gmail.com>
2024-11-17 19:23:39 -0500

    Created unit tests for creating menu items

--- app/menu/views.py
+++ app/menu/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.views import APIView
 from .models import MenuItem
 from .serializers import MenuItemSerializer
@@ -49,9 +49,9 @@ class MenuItemsView(APIView):
     def post(self,request, *args, **kwargs):
         """To create menu item"""
         try:
-            if not request.user.is_staff or request.user.is_admin or request.user.is_superuser:
+            if not (request.user.is_admin or request.user.is_superuser):
                 return Response(
-                    {"Error": "Only staff memmbers can create menu items"},
+                    {"Error": "Only admin/super user's can create menu items"},
                     status = status.HTTP_403_FORBIDDEN,
             )
 
@@ -59,7 +59,7 @@ class MenuItemsView(APIView):
 
             if serializer.is_valid():
                 serializer.save(item_upd_usr_id=request.user,item_upd_usr_email=request.user.email)
-                return Response(serializer.data,status=HTTP_200_OK)
+                return Response(serializer.data,status=HTTP_201_CREATED)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
